60a2ab811fb1c14dec451aaf1d78b668d6d2989b
brunofborges <brunofreitas.ads@gmail.com>
2023-06-05 09:02:55 -0300

    Fixed lambda copy of the templates

--- copy-templates.js
+++ copy-templates.js
@@ -5,9 +5,25 @@ const path = require('path');
 const sourceDir = path.join(__dirname, 'templates');
 const destinationDir = path.join(__dirname, 'dist', 'templates');
 
-ncp(sourceDir, destinationDir, function (err) {
-  if (err) {
-    return console.error(err);
-  }
-  console.log('Templates copied successfully!');
-});
+// Resolve the correct paths for deployment environment
+if (process.env.LAMBDA_TASK_ROOT) {
+  // Running on AWS Lambda
+  const rootDir = process.env.LAMBDA_TASK_ROOT;
+  const deploymentSourceDir = path.join(rootDir, 'src', 'templates');
+  const deploymentDestinationDir = path.join(rootDir, 'dist', 'templates');
+
+  ncp(deploymentSourceDir, deploymentDestinationDir, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Templates copied successfully!');
+  });
+} else {
+  // Running on local or other environment
+  ncp(sourceDir, destinationDir, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Templates copied successfully!');
+  });
+}
